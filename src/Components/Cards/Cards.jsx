b5457c4e108d5b194addd5dd91d15a80b89572a6
IvanVelazquezz98 <Velazquez.ivan.e98@gmail.com>
2022-09-29 01:14:50 -0300

    Scroll

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,11 +5,11 @@ export default function Card({ nombre,razon_social,nit,telefono,codigo }){
         <div >
             
             <div >
-                <h3 > {nombre ? nombre : <p>Loading..</p>}</h3>
-                <h5 >{razon_social ? razon_social : <p>Loading..</p>}</h5>
-                <h5 >{nit ? nit : <p>Loading..</p>}</h5>
-                <h5 >{telefono ? telefono : <p>Loading..</p>}</h5>
-                <h5 >{codigo ? codigo : <p>Loading..</p>}</h5>
+                <h3 >Nombre: {nombre ? nombre : <p>Loading..</p>}</h3>
+                <h5 >Razon Social:{razon_social ? razon_social : <p>Loading..</p>}</h5>
+                <h5 >Nit:{nit ? nit : <p>Loading..</p>}</h5>
+                <h5 >Telefono:{telefono ? telefono : <p>Loading..</p>}</h5>
+                <h5 >Codigo:{codigo ? codigo : <p>Loading..</p>}</h5>
                 
             </div>
         </div>
